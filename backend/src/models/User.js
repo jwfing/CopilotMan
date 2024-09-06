956ee111ffb086731171f4f09696e7e3e93abfb2
fengjunwen <fengjunwen@keep.com>
2024-09-06 10:48:52 +0800

    add createdAt field to User model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -19,6 +19,12 @@ const User = sequelize.define('User', {
     type: DataTypes.BOOLEAN,
     defaultValue: false,
   },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,  // 这将自动设置为创建记录时的时间
+  }
+}, {
+  timestamps: true,  // 这将自动添加 createdAt 和 updatedAt 字段
 });
 
 module.exports = User;
